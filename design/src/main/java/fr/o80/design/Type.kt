e5892e9aa87bdb9b8e4f8a09df74fd3221768c68
Olivier PEREZ <olivier@olivierperez.fr>
2022-09-24 22:58:23 +0200

    Adjust sizes

--- design/src/main/java/fr/o80/design/Type.kt
+++ design/src/main/java/fr/o80/design/Type.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     h1 = TextStyle(
         fontWeight = FontWeight.Light,
@@ -17,6 +16,21 @@ val Typography = Typography(
         fontWeight = FontWeight.Light,
         fontSize = 60.sp,
         letterSpacing = (-0.5).sp
+    ),
+    h3 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 48.sp,
+        letterSpacing = 0.sp
+    ),
+    h4 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
+    h5 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        letterSpacing = 0.sp
     ),
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
