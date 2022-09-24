aa0da96265650892dcdff1e6e693f4f421e99f2f
Olivier PEREZ <olivier@olivierperez.fr>
2022-09-24 21:13:48 +0200

    Initial sources

---
+++ app/src/main/java/fr/o80/puitsvieux/MainActivity.kt
@@ -0,0 +1,44 @@
+package fr.o80.puitsvieux
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import fr.o80.design.PuitsVieuxTheme
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PuitsVieuxTheme(darkTheme = true) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PuitsVieuxTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/fr/o80/puitsvieux/PuitsVieuxApplication.kt
@@ -0,0 +1,7 @@
+package fr.o80.puitsvieux
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PuitsVieuxApplication : Application()
