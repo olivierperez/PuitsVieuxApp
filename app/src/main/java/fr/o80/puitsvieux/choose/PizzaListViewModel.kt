b297a310f0e69a95842f5ce710e4aefe5309edcd
Olivier PEREZ <olivier@olivierperez.fr>
2022-09-26 21:12:47 +0200

    clean

--- app/src/main/java/fr/o80/puitsvieux/choose/PizzaListViewModel.kt
+++ app/src/main/java/fr/o80/puitsvieux/choose/PizzaListViewModel.kt
@@ -7,9 +7,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import fr.o80.puitsvieux.data.Pizza
 import fr.o80.puitsvieux.data.PizzaListProvider
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -23,17 +21,19 @@ class PizzaListViewModel @Inject constructor(
 
     private val _state: MutableStateFlow<PizzaListState> = MutableStateFlow(PizzaListState.empty())
 
-    val state: StateFlow<PizzaListState> = _state.stateIn(
-        viewModelScope,
-        SharingStarted.WhileSubscribed(5000),
-        PizzaListState.empty()
-    )
+    val state: StateFlow<PizzaListState> = _state
 
     init {
         viewModelScope.launch {
             try {
                 val pizzas = pizzaListProvider.fetch()
-                _state.update { it.copy(pizzas = pizzas, hasError = false, canCallPizzeria = true) }
+                _state.update {
+                    it.copy(
+                        pizzas = pizzas,
+                        hasError = false,
+                        canCallPizzeria = true
+                    )
+                }
             } catch (e: Exception) {
                 Log.e(TAG, "Failed to get pizzas", e)
                 _state.update {
